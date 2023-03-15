9b0c58389efc006ce6a5fe422d72397db243ab00
alex <alexandru.andrei@colorado.edu>
2023-03-15 19:22:53 -0400

    first time crud app

---
+++ src/main/java/lol/homestead/homeapp/repository/ShoppingRepository.java
@@ -0,0 +1,9 @@
+package lol.homestead.homeapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShoppingRepository extends JpaRepository<ShoppingRepository, Integer> {
+
+
+
+}
